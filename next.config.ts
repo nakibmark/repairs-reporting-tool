aea700fa60f253f017f68a116c20f8973f8efc70
Mark Nakib <mark@marknakib.com>
2025-04-18 11:34:46 -0400

    Initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
-export default {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-        search: ''
+        port: '', // Optional: Defaults to '' if not specified
+        pathname: '/u/**' // Allows any path starting with /u/
       },
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
-        search: ''
+        port: '', // Optional
+        pathname: '/**' // Allows any path under this hostname
       }
     ]
   }
 };
+
+export default nextConfig;
