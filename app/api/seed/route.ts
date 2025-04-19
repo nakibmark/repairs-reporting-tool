7736650e6a5b402d755877a05f7f32d498697335
Mark Nakib <mark@marknakib.com>
2025-04-18 22:25:22 -0400

    Improve seed data

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -8,54 +8,117 @@ export async function GET() {
   await reset(db, schema);
   await seed(db, schema).refine((f) => ({
     partners: {
-      count: 2,
+      count: 200,
+      with: {
+        users: 3
+      },
       columns: {
-        name: f.companyName(),
+        partnerNo: f.int({
+          minValue: 100000,
+          maxValue: 999999
+        }),
+        partnerName: f.companyName({
+          isUnique: true
+        }),
         numberWatchmakers: f.int({
           minValue: 0,
-          maxValue: 10
+          maxValue: 100
+        }),
+        city: f.city(),
+        state: f.state(),
+        country: f.country(),
+        market: f.valuesFromArray({
+          values: ['US', 'LATAM', 'MX']
+        }),
+        region: f.valuesFromArray({
+          values: ['US', 'LATAM', 'MX']
         })
-      },
-      with: {
-        users: 2
+      }
+    },
+    users: {
+      columns: {
+        name: f.fullName(),
+        phone: f.phoneNumber(),
+        role: f.default({
+          defaultValue: 'dealer'
+        })
       }
     },
     reports: {
-      count: 5,
+      count: 10,
       with: {
-        reportItems: 10
+        reportItems: 20
       },
       columns: {
         isSubmitted: f.default({
           defaultValue: false
         }),
-        reportYear: f.year(),
+        reportYear: f.int({
+          minValue: 2023,
+          maxValue: 2050
+        }),
         reportMonth: f.int({
           minValue: 1,
           maxValue: 12
         })
       }
     },
+    reportItems: {
+      columns: {
+        repairNo: f.int({
+          minValue: 100000,
+          maxValue: 999999
+        }),
+        article: f.int({
+          minValue: 100000,
+          maxValue: 999999
+        }),
+        serialNo: f.string(),
+        comments: f.loremIpsum({
+          sentencesCount: 1
+        })
+      }
+    },
     brands: {
-      count: 3,
+      count: 4,
       columns: {
         name: f.valuesFromArray({
-          values: ['Cartier', 'Vacheron Constantin', 'Panerai'],
+          values: [
+            'Cartier',
+            'Vacheron Constantin',
+            'Panerai',
+            'A. Lange and Sohne'
+          ],
           isUnique: true
         })
       }
     },
-    serviceLevelTypesTypes: {
+    serviceLevelTypes: {
+      count: 6,
       columns: {
         isActive: f.default({
           defaultValue: true
+        }),
+        name: f.valuesFromArray({
+          values: [
+            'Battery change',
+            'Maintenance',
+            'Repair',
+            'Level 1',
+            'Level 2',
+            'Other'
+          ]
         })
       }
     },
     warrantyTypes: {
+      count: 3,
       columns: {
         isActive: f.default({
           defaultValue: true
+        }),
+        name: f.valuesFromArray({
+          values: ['Sales Warranty', 'Service Warranty', 'Gift Warranty']
         })
       }
     },
