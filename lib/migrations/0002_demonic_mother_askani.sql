e5766a7738abcc2074e21c2361d2714c5224421a
Mark Nakib <mark@marknakib.com>
2025-04-29 13:56:25 -0400

    Remove migrations folder

--- lib/migrations/0002_demonic_mother_askani.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "report_items" DROP CONSTRAINT "report_items_brandId_brands_id_fk";
---> statement-breakpoint
-ALTER TABLE "report_items" ALTER COLUMN "brandId" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "report_items" ADD CONSTRAINT "report_items_brandId_brands_id_fk" FOREIGN KEY ("brandId") REFERENCES "public"."brands"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "idx_brands_name" ON "brands" USING btree ("name");
