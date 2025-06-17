b32f458b5704b7b5a09f3373dc3d50ba2a1b1135
Jove Luna <joveluna@gmail.com>
2025-06-17 11:44:00 -0400

    Starting conditional rendering of rows based on editing

--- app/(dashboard)/reports/[id]/report-item.tsx
+++ app/(dashboard)/reports/[id]/report-item.tsx
@@ -12,57 +12,120 @@ import { ReportItemWithNames} from '@/lib/data/reportItems';
 import {deleteReportItem, editReportItem} from "./actions";
 
 export const ReportItem = ({ item }: { item: ReportItemWithNames }) => {
-  return (
-    <TableRow>
-      <TableCell className="hidden md:table-cell">
-        {item.dateIn.toLocaleDateString('en-US')}
-      </TableCell>
-      <TableCell className="hidden md:table-cell">{item.brand.name}</TableCell>
-      <TableCell className="hidden md:table-cell">{item.repairNo}</TableCell>
-      <TableCell className="hidden md:table-cell">{item.article}</TableCell>
-      <TableCell className="hidden max-w-64 md:table-cell truncate">
-        {item.serialNo}
-      </TableCell>
-      <TableCell className="hidden md:table-cell">
-        {item.warrantyType.name}
-      </TableCell>
-      <TableCell className="hidden md:table-cell">
-        {item.serviceLevelType.name}
-      </TableCell>
-      <TableCell>
-        {item.dateOut ? item.dateOut.toLocaleDateString('en-US') : ''}
-      </TableCell>
-      <TableCell>{item.comments}</TableCell>
-      <TableCell>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button aria-haspopup="true" size="icon" variant="ghost">
-              <MoreHorizontal className="h-4 w-4" />
-              <span className="sr-only">Toggle menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem>
-              <form>
-                <input type="hidden" name="id" value={item.id} />
-                <button type="button" onClick={() => handleEditClick(item.id)}>Edit</button>
-              </form>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <form>
-                <button type="button" onClick={() => handleDeleteClick(item.id)}>Delete</button>
-              </form>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </TableCell>
-    </TableRow>
-  );
+  let isEditing: boolean = true;
+  if(isEditing){
+    return (
+      <TableRow>
+        <TableCell className="hidden md:table-cell">
+          {item.dateIn.toLocaleDateString('en-US')}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">{item.brand.name}</TableCell>
+        <TableCell className="hidden md:table-cell">{item.repairNo}</TableCell>
+        <TableCell className="hidden md:table-cell">{item.article}</TableCell>
+        <TableCell className="hidden max-w-64 md:table-cell truncate">
+          {item.serialNo}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">
+          {item.warrantyType.name}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">
+          {item.serviceLevelType.name}
+        </TableCell>
+        <TableCell>
+          {item.dateOut ? item.dateOut.toLocaleDateString('en-US') : ''}
+        </TableCell>
+        <TableCell>{item.comments}</TableCell>
+        <TableCell>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-haspopup="true" size="icon" variant="ghost">
+                <MoreHorizontal className="h-4 w-4" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleSaveClick(item)}>
+                    Save
+                  </button>
+                </form>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleCancelClick(item.id)}>
+                    Cancel
+                  </button>
+                </form>
+              </DropdownMenuItem> 
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </TableCell>
+     </TableRow>
+    );
+  }else{
+    return (
+      <TableRow>
+        <TableCell className="hidden md:table-cell">
+          {item.dateIn.toLocaleDateString('en-US')}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">{item.brand.name}</TableCell>
+        <TableCell className="hidden md:table-cell">{item.repairNo}</TableCell>
+        <TableCell className="hidden md:table-cell">{item.article}</TableCell>
+        <TableCell className="hidden max-w-64 md:table-cell truncate">
+          {item.serialNo}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">
+          {item.warrantyType.name}
+        </TableCell>
+        <TableCell className="hidden md:table-cell">
+          {item.serviceLevelType.name}
+        </TableCell>
+        <TableCell>
+          {item.dateOut ? item.dateOut.toLocaleDateString('en-US') : ''}
+        </TableCell>
+        <TableCell>{item.comments}</TableCell>
+        <TableCell>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-haspopup="true" size="icon" variant="ghost">
+                <MoreHorizontal className="h-4 w-4" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleEditClick(item.id)}>
+                    Edit
+                  </button>
+                </form>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleDeleteClick(item.id)}>
+                    Delete
+                  </button>
+                </form>
+              </DropdownMenuItem> 
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </TableCell>
+     </TableRow>
+    );
+  }
 };
 async function handleDeleteClick(id: string){
   await deleteReportItem(id);
 }
 async function handleEditClick(id: string){
   await editReportItem(id);
+}
+async function handleSaveClick(Item: ReportItemWithNames){
+
+}
+async function handleCancelClick(id: string){
+
 }
