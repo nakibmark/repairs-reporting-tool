a04c8bb194cb67d6e2bc77c7ad5e401c5a5f1c76
Jove Luna <joveluna@gmail.com>
2025-06-18 11:40:09 -0400

    restructured table rows to use child reportItemCell components dynamically rendered based on state of parent component

--- app/(dashboard)/reports/[id]/report-item-cell.tsx
+++ app/(dashboard)/reports/[id]/report-item-cell.tsx
@@ -1,22 +1,103 @@
 import { TableCell} from '@/components/ui/table';
 import { ReportItemWithNames} from '@/lib/data/reportItems';
+import { MoreHorizontal } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
 import React from 'react';
+
 type handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: keyof ReportItemWithNames) => void;
-export const ReportItemCell = ({editState, handleChange, cellName, editedItem, item}: {editState:boolean, handleChange:handleChange, cellName:string, editedItem:any, item:ReportItemWithNames}) => {
-    if (editState){
+type handleSaveClick = (item: ReportItemWithNames) => void;
+type handleCancelClick = (id: string) => void;
+type handleEditClick = (id: string, isEditing:boolean) => void;
+type handleDeleteClick = (id: string) => void;
+
+export const ReportItemCell = ({editState, handleChange, cellName, valueEdit, valueStatic}: {editState:boolean, handleChange:handleChange, cellName:string, valueEdit:string, valueStatic:(string | null)}) => {
+  //dynamically render the reportItemCell depending on the state of the parent row
+  if (editState){
     return(
-        <TableCell className="hidden md:table-cell">
-                  <input
-                    value={editedItem.dateIn?.toLocaleDateString('en-US') || ''}
-                    onChange={(e) => handleChange(e, cellName as keyof ReportItemWithNames)}
-                  />
-        </TableCell>
+      <TableCell className="hidden md:table-cell">
+        <input
+          value={valueEdit}
+          onChange={(e) => handleChange(e, cellName as keyof ReportItemWithNames)}
+        />
+      </TableCell>
     )
   }else{
     return(
-        <TableCell className="hidden md:table-cell">
-                  
-        </TableCell>
+      <TableCell className="hidden md:table-cell">
+        {valueStatic}     
+      </TableCell>
     )
   }
+}
+
+export const ReportItemCellMenu = ({editState, handleSaveClick, handleCancelClick, handleEditClick, handleDeleteClick, editedItem, item}: {editState:boolean, handleSaveClick:handleSaveClick, handleCancelClick:handleCancelClick, handleEditClick:handleEditClick, handleDeleteClick:handleDeleteClick, editedItem:any, item:ReportItemWithNames}) => {
+   //dynamically render the menu depending on the state of the parent row
+   if(editState){
+    return(
+      <TableCell>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-haspopup="true" size="icon" variant="ghost">
+                <MoreHorizontal className="h-4 w-4" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleSaveClick(editedItem)}>
+                    Save
+                  </button>
+                </form>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleCancelClick(item.id)}>
+                    Cancel
+                  </button>
+                </form>
+              </DropdownMenuItem> 
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </TableCell>
+    );
+   }else{
+    return(
+      <TableCell>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-haspopup="true" size="icon" variant="ghost">
+                <MoreHorizontal className="h-4 w-4" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleEditClick(item.id, editState)}>
+                    Edit
+                  </button>
+                </form>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <form>
+                  <button type="button" onClick={() => handleDeleteClick(item.id)}>
+                    Delete
+                  </button>
+                </form>
+              </DropdownMenuItem> 
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </TableCell>
+    );
+   }
 }
--- app/(dashboard)/reports/[id]/report-item.tsx
+++ app/(dashboard)/reports/[id]/report-item.tsx
@@ -10,7 +10,7 @@ import { MoreHorizontal } from 'lucide-react';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { ReportItemWithNames} from '@/lib/data/reportItems';
 import {deleteReportItem, editReportItem, saveReportItem, cancelEditReportItem} from "./actions";
-import { ReportItemCell } from './report-item-cell';
+import { ReportItemCell, ReportItemCellMenu } from './report-item-cell';
 import React from 'react';
 
 
@@ -22,149 +22,25 @@ export const ReportItem = ({ item }: { item: ReportItemWithNames }) => {
   };
     return (
       <TableRow>
-        <ReportItemCell handleChange={handleChange} editedItem={editedItem} editState={isEditing} cellName='dateIn' item={item}></ReportItemCell>
-        <TableCell className="hidden md:table-cell">
-          <input
-            type="text"
-            value={editedItem.brand?.name || ''}
-            onChange={(e) => handleChange(e, 'brand')}
-          />
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          <input
-            type="text"
-            value={editedItem.repairNo || ''}
-            onChange={(e) => handleChange(e, 'repairNo')}
-          />
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          <input
-            type="text"
-            value={editedItem.article || ''}
-            onChange={(e) => handleChange(e, 'article')}
-          />
-        </TableCell>
-        <TableCell className="hidden max-w-64 md:table-cell truncate">
-          <input
-            type="text"
-            value={editedItem.serialNo || ''}
-            onChange={(e) => handleChange(e, 'serialNo')}
-          />
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          <input
-            type="text"
-            value={editedItem.warrantyType?.name || ''}
-            onChange={(e) => handleChange(e, 'warrantyType')}
-          />
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          <input
-            type="text"
-            value={editedItem.serviceLevelType?.name || ''}
-            onChange={(e) => handleChange(e, 'serviceLevelType')}
-          />
-        </TableCell>
-        <TableCell>
-          <input
-            type="text"
-            value={editedItem.dateOut?.toLocaleDateString('en-US') || ''}
-            onChange={(e) => handleChange(e, 'dateOut')}
-          />
-        </TableCell>
-        <TableCell>
-          <textarea
-            value={editedItem.comments || ''}
-            onChange={(e) => handleChange(e, 'comments')}
-          />
-        </TableCell>
-        <TableCell>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button aria-haspopup="true" size="icon" variant="ghost">
-                <MoreHorizontal className="h-4 w-4" />
-                <span className="sr-only">Toggle menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem>
-                <form>
-                  <button type="button" onClick={() => handleSaveClick(editedItem)}>
-                    Save
-                  </button>
-                </form>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <form>
-                  <button type="button" onClick={() => handleCancelClick(item.id)}>
-                    Cancel
-                  </button>
-                </form>
-              </DropdownMenuItem> 
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </TableCell>
-     </TableRow>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='dateIn' valueEdit={editedItem.dateIn?.toLocaleDateString('en-US') || ''} valueStatic={item.dateIn.toLocaleDateString('en-US')}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='brand' valueEdit={editedItem.brand?.name || ''} valueStatic={item.brand.name}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='repairNo' valueEdit={editedItem.repairNo || ''} valueStatic={item.repairNo}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='article' valueEdit={editedItem.article || ''} valueStatic={item.article}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='serialNo' valueEdit={editedItem.serialNo || ''} valueStatic={item.serialNo}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='warrantyType' valueEdit={editedItem.warrantyType?.name || ''} valueStatic={item.warrantyType.name}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='serviceLevelType' valueEdit={editedItem.serviceLevelType?.name || ''} valueStatic={item.serviceLevelType.name}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='dateOut' valueEdit={editedItem.dateOut?.toLocaleDateString('en-US') || ''} valueStatic={item.dateOut ? item.dateOut.toLocaleDateString('en-US') : ''}></ReportItemCell>
+        <ReportItemCell handleChange={handleChange} editState={isEditing} cellName='comments' valueEdit={editedItem.comments || ''} valueStatic={item.comments}></ReportItemCell>
+        <ReportItemCellMenu editState={isEditing} handleDeleteClick={handleDeleteClick} handleCancelClick={handleCancelClick} handleEditClick={handleEditClick} handleSaveClick={handleSaveClick} item={item} editedItem={editedItem}></ReportItemCellMenu>
+      </TableRow>
     );
-  /*
-    return (
-      <TableRow>
-        <TableCell className="hidden md:table-cell">
-          {item.dateIn.toLocaleDateString('en-US')}
-        </TableCell>
-        <TableCell className="hidden md:table-cell">{item.brand.name}</TableCell>
-        <TableCell className="hidden md:table-cell">{item.repairNo}</TableCell>
-        <TableCell className="hidden md:table-cell">{item.article}</TableCell>
-        <TableCell className="hidden max-w-64 md:table-cell truncate">
-          {item.serialNo}
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          {item.warrantyType.name}
-        </TableCell>
-        <TableCell className="hidden md:table-cell">
-          {item.serviceLevelType.name}
-        </TableCell>
-        <TableCell>
-          {item.dateOut ? item.dateOut.toLocaleDateString('en-US') : ''}
-        </TableCell>
-        <TableCell>{item.comments}</TableCell>
-        <TableCell>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button aria-haspopup="true" size="icon" variant="ghost">
-                <MoreHorizontal className="h-4 w-4" />
-                <span className="sr-only">Toggle menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem>
-                <form>
-                  <button type="button" onClick={() => handleEditClick(item.id)}>
-                    Edit
-                  </button>
-                </form>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <form>
-                  <button type="button" onClick={() => handleDeleteClick(item.id)}>
-                    Delete
-                  </button>
-                </form>
-              </DropdownMenuItem> 
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </TableCell>
-     </TableRow>
-    );*/
 };
 async function handleDeleteClick(id: string){
   await deleteReportItem(id);
 }
-async function handleEditClick(id: string){
+async function handleEditClick(id: string, isEditing:boolean){
+  isEditing = true;
   await editReportItem(id);
-
 }
 async function handleSaveClick(item: ReportItemWithNames){
   await saveReportItem(item);
