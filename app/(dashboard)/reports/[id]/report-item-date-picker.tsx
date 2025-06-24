f875ac3587779fde03f74bf438bd6c7d69c9948c
Mark Nakib <mark@marknakib.com>
2025-06-24 10:38:36 -0400

    Use string instead of Date for PG DATE type
    Having the DATE be parsed into a Date serverside created issues with timezones and such.
    Instead, we should keep the DATEs as yyyy-MM-dd format strings until we actually need them to be Date objects (like for the date picker calendar).

--- app/(dashboard)/reports/[id]/report-item-date-picker.tsx
+++ app/(dashboard)/reports/[id]/report-item-date-picker.tsx
@@ -1,20 +1,30 @@
-'use client'
+'use client';
 import { TableCell } from '@/components/ui/table';
-import * as React from "react"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
 import {
   Popover,
   PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+  PopoverTrigger
+} from '@/components/ui/popover';
+import { TZDate } from 'react-day-picker';
+import { format } from 'date-fns';
 
+const ReportItemDatePicker = ({
+  isEditing,
+  onChange,
+  value
+}: {
+  isEditing: boolean;
+  onChange: (date: string) => void;
+  value: string | undefined;
+}) => {
+  const [open, setOpen] = React.useState(false);
 
-const ReportItemDatePicker = ({ isEditing, onChange, value }: { isEditing: boolean, onChange: Function, value: Date | undefined }) => {
-  const [open, setOpen] = React.useState(false)
-  const [date, setDate] = React.useState<Date | undefined>(value)
+  const dateValue = value ? new TZDate(value, 'UTC') : undefined;
 
-  return isEditing ?
+  return isEditing ? (
     <TableCell className="hidden md:table-cell">
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
@@ -23,28 +33,27 @@ const ReportItemDatePicker = ({ isEditing, onChange, value }: { isEditing: boole
             id="date"
             className="w-48 justify-between font-normal"
           >
-            {date ? date.toLocaleDateString() : ''}
+            {value || 'Select Date'}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto overflow-hidden p-0" align="start">
           <Calendar
             mode="single"
-            selected={date}
-            defaultMonth={date}
+            selected={dateValue}
+            defaultMonth={dateValue}
+            timeZone="UTC"
             captionLayout="dropdown"
             onSelect={(date) => {
-              setDate(date)
-              onChange(date)
-              setOpen(false)
+              onChange(date ? format(date, 'yyyy-MM-dd') : '');
+              setOpen(false);
             }}
           />
         </PopoverContent>
       </Popover>
     </TableCell>
-    :
-    <TableCell className="hidden md:table-cell">
-      {value?.toLocaleDateString('en-US') || ''}
-    </TableCell>
-}
+  ) : (
+    <TableCell className="hidden md:table-cell">{value || '-'}</TableCell>
+  );
+};
 
-export default ReportItemDatePicker
+export default ReportItemDatePicker;
--- app/(dashboard)/reports/[id]/report-item.tsx
+++ app/(dashboard)/reports/[id]/report-item.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { TableRow } from '@/components/ui/table';
 import { ReportItemWithNames } from '@/lib/data/reportItems';
@@ -9,59 +9,131 @@ import { deleteReportItem, saveReportItem } from './actions';
 import ReportItemDropdown from './report-item-dropdown';
 import ReportItemDatePicker from './report-item-date-picker';
 
-const ReportItem = ({ item, brands, serviceLevelTypes, warrantyTypes }:
-  { item: ReportItemWithNames, brands: { id: number, name: string }[], serviceLevelTypes: { id: number, name: string }[], warrantyTypes: { id: number, name: string }[] }) => {
-  const [isEditing, setIsEditing] = React.useState(false)
+const ReportItem = ({
+  item,
+  brands,
+  serviceLevelTypes,
+  warrantyTypes
+}: {
+  item: ReportItemWithNames;
+  brands: { id: number; name: string }[];
+  serviceLevelTypes: { id: number; name: string }[];
+  warrantyTypes: { id: number; name: string }[];
+}) => {
+  const [isEditing, setIsEditing] = React.useState(false);
   const [editedItem, setEditedItem] = React.useState(item);
 
-  const handleChange = (field: keyof ReportItemWithNames) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setEditedItem({ ...editedItem, [field]: e.target.value });
-  };
+  const handleChange =
+    (field: keyof ReportItemWithNames) =>
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setEditedItem({ ...editedItem, [field]: e.target.value });
+    };
   const findBrandByID = (id: number): string => {
-    return brands.find(brand => brand.id === id)?.name || ""
-  }
+    return brands.find((brand) => brand.id === id)?.name || '';
+  };
   const findServiceByID = (id: number): string => {
-    return serviceLevelTypes.find(serviceLevelType => serviceLevelType.id === id)?.name || ""
-  }
+    return (
+      serviceLevelTypes.find((serviceLevelType) => serviceLevelType.id === id)
+        ?.name || ''
+    );
+  };
   const findWarrantyByID = (id: number): string => {
-    return warrantyTypes.find(warrantyType => warrantyType.id === id)?.name || ""
-  }
-
+    return (
+      warrantyTypes.find((warrantyType) => warrantyType.id === id)?.name || ''
+    );
+  };
 
   return (
     <TableRow>
       <ReportItemDatePicker
-        onChange={(selectedDate: Date) => { setEditedItem({ ...editedItem, dateIn: selectedDate }) }}
-        isEditing={isEditing} value={editedItem.dateIn}></ReportItemDatePicker>
-      <ReportItemDropdown isEditing={isEditing} currentValue={editedItem.brand?.name || ''} options={brands}
-        onChange={(selectedId: string) => { setEditedItem({ ...editedItem, brand: { id: Number(selectedId), name: findBrandByID(Number(selectedId)) } }) }}>
-      </ReportItemDropdown>
-      <ReportItemCell onChange={handleChange('repairNo')} isEditing={isEditing} value={editedItem.repairNo || ''} ></ReportItemCell>
-      <ReportItemCell onChange={handleChange('article')} isEditing={isEditing} value={editedItem.article || ''} ></ReportItemCell>
-      <ReportItemCell onChange={handleChange('serialNo')} isEditing={isEditing} value={editedItem.serialNo || ''} ></ReportItemCell>
-      <ReportItemDropdown isEditing={isEditing} currentValue={editedItem.warrantyType?.name || ''} options={warrantyTypes}
-        onChange={(selectedId: string) => { setEditedItem({ ...editedItem, warrantyType: { id: Number(selectedId), name: findWarrantyByID(Number(selectedId)) } }) }}>
-      </ReportItemDropdown>
-      <ReportItemDropdown isEditing={isEditing} currentValue={editedItem.serviceLevelType?.name || ''} options={serviceLevelTypes}
-        onChange={(selectedId: string) => { setEditedItem({ ...editedItem, serviceLevelType: { id: Number(selectedId), name: findServiceByID(Number(selectedId)) } }) }}>
-      </ReportItemDropdown>
+        onChange={(selectedDate: string) => {
+          setEditedItem({ ...editedItem, dateIn: selectedDate });
+        }}
+        isEditing={isEditing}
+        value={editedItem.dateIn}
+      ></ReportItemDatePicker>
+      <ReportItemDropdown
+        isEditing={isEditing}
+        currentValue={editedItem.brand?.name || ''}
+        options={brands}
+        onChange={(selectedId: string) => {
+          setEditedItem({
+            ...editedItem,
+            brand: {
+              id: Number(selectedId),
+              name: findBrandByID(Number(selectedId))
+            }
+          });
+        }}
+      ></ReportItemDropdown>
+      <ReportItemCell
+        onChange={handleChange('repairNo')}
+        isEditing={isEditing}
+        value={editedItem.repairNo || ''}
+      ></ReportItemCell>
+      <ReportItemCell
+        onChange={handleChange('article')}
+        isEditing={isEditing}
+        value={editedItem.article || ''}
+      ></ReportItemCell>
+      <ReportItemCell
+        onChange={handleChange('serialNo')}
+        isEditing={isEditing}
+        value={editedItem.serialNo || ''}
+      ></ReportItemCell>
+      <ReportItemDropdown
+        isEditing={isEditing}
+        currentValue={editedItem.warrantyType?.name || ''}
+        options={warrantyTypes}
+        onChange={(selectedId: string) => {
+          setEditedItem({
+            ...editedItem,
+            warrantyType: {
+              id: Number(selectedId),
+              name: findWarrantyByID(Number(selectedId))
+            }
+          });
+        }}
+      ></ReportItemDropdown>
+      <ReportItemDropdown
+        isEditing={isEditing}
+        currentValue={editedItem.serviceLevelType?.name || ''}
+        options={serviceLevelTypes}
+        onChange={(selectedId: string) => {
+          setEditedItem({
+            ...editedItem,
+            serviceLevelType: {
+              id: Number(selectedId),
+              name: findServiceByID(Number(selectedId))
+            }
+          });
+        }}
+      ></ReportItemDropdown>
       <ReportItemDatePicker
-        onChange={(selectedDate: Date) => { setEditedItem({ ...editedItem, dateOut: selectedDate }) }}
-        isEditing={isEditing} value={editedItem.dateOut || undefined}></ReportItemDatePicker>
-      <ReportItemCell onChange={handleChange('comments')} isEditing={isEditing} value={editedItem.comments || ''} ></ReportItemCell>
+        onChange={(selectedDate: string) => {
+          setEditedItem({ ...editedItem, dateOut: selectedDate });
+        }}
+        isEditing={isEditing}
+        value={editedItem.dateOut || undefined}
+      ></ReportItemDatePicker>
+      <ReportItemCell
+        onChange={handleChange('comments')}
+        isEditing={isEditing}
+        value={editedItem.comments || ''}
+      ></ReportItemCell>
       <ReportItemEditMenu
         isEditing={isEditing}
         handleEditClick={() => setIsEditing(true)}
         handleCancelClick={() => {
-          setEditedItem(item)
-          setIsEditing(false)
+          setEditedItem(item);
+          setIsEditing(false);
         }}
         handleDeleteClick={async () => await deleteReportItem(item.id)}
         handleSaveClick={async () => {
-          await saveReportItem(editedItem)
-          setIsEditing(false)
-        }}>
-      </ReportItemEditMenu>
+          await saveReportItem(editedItem);
+          setIsEditing(false);
+        }}
+      ></ReportItemEditMenu>
     </TableRow>
   );
 };
