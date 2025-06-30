640902de51c372daac1e08f1b23633110760ba93
Mark Nakib <mark@marknakib.com>
2025-06-30 12:42:27 -0400

    Update reportId dynamic segment name (#6)
    Previously, we had just used `id` as the dynamic segment name for reports. This was getting confusing when referencing that path segment in client components because we have many IDs for many data types. Changing this segment name disambiguates it.

--- app/(dashboard)/reports/[reportId]/actions.ts
+++ app/(dashboard)/reports/[reportId]/actions.ts
@@ -2,12 +2,12 @@
 import { revalidatePath } from 'next/cache';
 import { deleteReportItemById, ReportItemWithNames, saveReportItemByItem } from '@/lib/data/reportItems';
 
-export async function deleteReportItem(id: string) {
-  await deleteReportItemById(id);
-  revalidatePath('/');
+export async function deleteReportItem(itemId: string) {
+  await deleteReportItemById(itemId);
+  revalidatePath('/reports/[reportId]/page', 'page');
 };
 
 export async function saveReportItem(item: ReportItemWithNames) {
   await saveReportItemByItem(item);
-  revalidatePath('/');
+  revalidatePath('/reports/[reportId]/page', 'page');
 };
