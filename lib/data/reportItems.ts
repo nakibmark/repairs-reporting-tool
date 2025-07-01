b578303e3a8fb5f04d74c1b76e662d82f3297423
Jove Luna <joveluna@gmail.com>
2025-07-01 13:21:41 -0400

    Removed unused import

--- lib/data/reportItems.ts
+++ lib/data/reportItems.ts
@@ -1,5 +1,5 @@
 import { db } from '../db';
-import { reportItems, SelectReportItem, InsertReportItem} from '../schema';
+import { reportItems, SelectReportItem} from '../schema';
 import { eq } from 'drizzle-orm';
 
 
