b3709ea519ec207d71e5c0cc178d81f555a591e0
Jove Luna <joveluna@gmail.com>
2025-07-01 11:22:13 -0400

    Removed other references from actions.ts

--- app/(dashboard)/actions.ts
+++ app/(dashboard)/actions.ts
@@ -1,10 +1,4 @@
 'use server';
 
-import { deleteProductById } from '@/lib/db-old';
 import { revalidatePath } from 'next/cache';
 
-export async function deleteProduct(formData: FormData) {
-  let id = Number(formData.get('id'));
-  await deleteProductById(id);
-  revalidatePath('/');
-}
