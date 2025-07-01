ab8b84d9df69b91cd10d957b6b647273e3203573
Jove Luna <joveluna@gmail.com>
2025-07-01 13:41:42 -0400

    Added function to create a report into actions layer

--- app/(dashboard)/reports/actions.ts
+++ app/(dashboard)/reports/actions.ts
@@ -1,9 +1,15 @@
 'use server';
-import { deleteReport } from '@/lib/data/reports';
+import { deleteReport , createReport} from '@/lib/data/reports';
+import { SelectReport } from '@/lib/schema';
 
 import { revalidatePath } from 'next/cache';
 
 export async function deleteReportById(reportId: number) {
     await deleteReport(reportId);
+    revalidatePath('/reports', 'page');
+};
+
+export async function CreateReportByReport(report: SelectReport) {
+    await createReport(report);
     revalidatePath('/reports', 'page');
 };
