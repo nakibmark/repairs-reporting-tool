275fa1bca0e27096fad824eda823bd01ecce1b75
Jove Luna <joveluna@gmail.com>
2025-07-01 09:26:21 -0400

    Cleaning up UI

--- app/(dashboard)/reports/[reportId]/report-items-table.tsx
+++ app/(dashboard)/reports/[reportId]/report-items-table.tsx
@@ -35,10 +35,13 @@ const ReportItemsTable = ({
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Report Details</CardTitle>
-        <CardDescription>View your report details.</CardDescription>
+      <div className="flex items-center">
+        <div>
+          <CardTitle>Report Details</CardTitle>
+          <CardDescription>View your report details.</CardDescription>
+        </div>
+        <div className="ml-auto flex items-center gap-2">
         <CardAction>
-          
           <Button type="button" onClick={() => setIsCreatingNewItem(true)} size="sm" className="h-8 gap-1">
           <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
@@ -46,6 +49,8 @@ const ReportItemsTable = ({
             </span>
           </Button>
         </CardAction>
+        </div>
+      </div>
       </CardHeader>
       <CardContent>
         <Table>
