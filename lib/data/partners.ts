a4b7ca3512da619c27c4602529a0622b686486f8
Jove Luna <joveluna@gmail.com>
2025-07-07 14:00:07 -0400

    Added function to handle deletion of partner into data layer

--- lib/data/partners.ts
+++ lib/data/partners.ts
@@ -1,6 +1,7 @@
 'use server';
 import { db } from '../db';
 import { partners } from '../schema';
+import { eq } from 'drizzle-orm';
 
 export const selectPartnersOptions = async () =>
   await db
@@ -11,3 +12,6 @@ export const selectPartnersOptions = async () =>
     .from(partners);
 
 export const selectPartners = async () => await db.select().from(partners);
+
+export const deletePartnerById = async (partnerId: number) =>
+  await db.delete(partners).where(eq(partners.id, partnerId));
