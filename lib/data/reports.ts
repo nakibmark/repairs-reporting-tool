77b5c30af18fa1f4db7a87669c538cc752500a9e
Jove Luna <joveluna@gmail.com>
2025-07-14 16:48:01 -0400

    Refactored selectReports method to be compatible with pagination

--- lib/data/reports.ts
+++ lib/data/reports.ts
@@ -1,37 +1,32 @@
 'use server';
 import { db } from '../db';
-import { InsertReport, reports, SelectReport } from '../schema';
-import { count, eq } from 'drizzle-orm';
+import { InsertReport, reports } from '../schema';
+import { eq } from 'drizzle-orm';
 
 export async function selectReports(
-  offset: number,
-  partnerId?: string
-): Promise<{
-  reports: SelectReport[];
-  newOffset: number | null;
-  totalReports: number;
-}> {
-  if (offset === null) {
-    return { reports: [], newOffset: null, totalReports: 0 };
-  }
-
-  const totalReports = await db
-    .select({ count: count() })
-    .from(reports)
-    .where(partnerId ? eq(reports.partnerId, Number(partnerId)) : undefined);
-  const moreReports = await db
-    .select()
-    .from(reports)
-    .where(partnerId ? eq(reports.partnerId, Number(partnerId)) : undefined)
-    .limit(20)
-    .offset(offset);
-  const newOffset = moreReports.length >= 20 ? offset + 20 : null;
-
-  return {
-    reports: moreReports,
-    newOffset,
-    totalReports: totalReports[0].count,
-  };
+  partnerId?: string,
+  reportsPerPage?: number,
+  currentPage?: number
+) {
+  const offset =
+    currentPage && reportsPerPage ? (currentPage - 1) * reportsPerPage : null;
+  const results =
+    reportsPerPage && offset
+      ? await db
+          .select()
+          .from(reports)
+          .where(
+            partnerId ? eq(reports.partnerId, Number(partnerId)) : undefined
+          )
+          .limit(reportsPerPage)
+          .offset(offset)
+      : await db
+          .select()
+          .from(reports)
+          .where(
+            partnerId ? eq(reports.partnerId, Number(partnerId)) : undefined
+          );
+  return results;
 }
 
 export async function deleteReport(id: number) {
