b751ef70aa7f2b1d369b43066d017df09fe12fbf
Jove Luna <joveluna@gmail.com>
2025-07-15 11:52:05 -0400

    Removed unused import

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -14,7 +14,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { useDebouncedCallback } from 'use-debounce';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
