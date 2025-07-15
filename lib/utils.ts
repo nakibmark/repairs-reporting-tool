910acedbef11e14e39a6afb69bb44eaf7d3f9959
Jove Luna <joveluna@gmail.com>
2025-07-15 09:52:38 -0400

    Fixed pagation when page <= 3 or 3 away from max page

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,10 +10,18 @@ export function generatePagination(currentPage: number, totalPages: number) {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
   if (currentPage <= 3) {
-    return [1, 2, 3, '...', totalPages - 1, totalPages];
+    return [1, 2, 3, 4, '...', totalPages - 1, totalPages];
   }
   if (currentPage >= totalPages - 2) {
-    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
+    return [
+      1,
+      2,
+      '...',
+      totalPages - 3,
+      totalPages - 2,
+      totalPages - 1,
+      totalPages,
+    ];
   }
   return [
     1,
