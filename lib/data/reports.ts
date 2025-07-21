6555ef6d1d24b0729a34b9e3b4495bb9f28f8ac3
Mark Nakib <mark@marknakib.com>
2025-07-21 12:42:51 -0400

    Clean up report and reportItem data queries and prepare statements

--- lib/data/reports.ts
+++ lib/data/reports.ts
@@ -1,11 +1,11 @@
 'use server';
 import { db } from '../db';
 import { InsertReport, reports } from '../schema';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 
 export async function selectReports(
-  reportsPerPage: number,
   currentPage: number,
+  reportsPerPage: number,
   partnerId?: string
 ) {
   const offset = (currentPage - 1) * reportsPerPage;
@@ -13,10 +13,26 @@ export async function selectReports(
     .select()
     .from(reports)
     .where(partnerId ? eq(reports.partnerId, Number(partnerId)) : undefined)
+    .orderBy(reports.submissionPeriodClosesAt)
     .limit(reportsPerPage)
     .offset(offset);
   return results;
 }
+
+const preparedFindReportStatusById = db.query.reports
+  .findFirst({
+    where: eq(reports.id, sql.placeholder('id')),
+    columns: { isSubmitted: true },
+  })
+  .prepare('find_report_status_by_id');
+
+export const selectReportStatusById = async (id: number) =>
+  await preparedFindReportStatusById.execute({ id }).then((result) => {
+    if (!result) {
+      throw new Error(`Report for ID ${id} not found`);
+    }
+    return result.isSubmitted;
+  });
 
 export async function deleteReport(id: number) {
   await db.delete(reports).where(eq(reports.id, id));
@@ -26,24 +42,6 @@ export async function insertReport(report: InsertReport) {
   return await db.insert(reports).values(report).returning();
 }
 
-export async function selectReportStatusById(id: number) {
-  return await db.query.reports.findFirst({
-    where: eq(reports.id, id),
-    columns: { isSubmitted: true },
-  });
-}
-
 export async function updateReportStatusById(id: number, isSubmitted: boolean) {
   await db.update(reports).set({ isSubmitted }).where(eq(reports.id, id));
-}
-
-export async function selectTotalPages(
-  reportsPerPage: number,
-  partnerId?: number
-) {
-  const results = partnerId
-    ? await db.select().from(reports).where(eq(reports.partnerId, partnerId))
-    : await db.select().from(reports);
-
-  return Math.ceil(results.length / reportsPerPage);
 }
