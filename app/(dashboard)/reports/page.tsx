ebd695f69f0b0fb34ddfa776ee0775bcf8bffce7
Jove Luna <joveluna@gmail.com>
2025-07-28 14:10:43 -0400

    Removed old server side filtering code from reports page

--- app/(dashboard)/reports/page.tsx
+++ app/(dashboard)/reports/page.tsx
@@ -13,23 +13,13 @@ export default async function ReportsPage({
 }) {
   const cookieStore = await cookies();
   const partnerId = cookieStore.get('partnerId')?.value;
-  const {
-    page = '1',
-    size = '10',
-    submitted,
-  } = await searchParams.then(flattenSearchParams);
-  let submittedFilter: boolean | undefined;
-  if (submitted === 'submitted') {
-    submittedFilter = true;
-  } else if (submitted === 'draft') {
-    submittedFilter = false;
-  }
+  const { page = '1', size = '10' } =
+    await searchParams.then(flattenSearchParams);
 
   const { reports, totalPages } = await selectReports({
     currentPage: +page,
     limit: +size,
     query: partnerId,
-    submittedFilter: submittedFilter,
   });
 
   return (
