39b77acf3eca65551addb9ee54fb742adb9d23ff
Jove Luna <joveluna@gmail.com>
2025-08-04 10:29:34 -0400

    Table displaying the brands

--- app/(dashboard)/admin/brands/page.tsx
+++ app/(dashboard)/admin/brands/page.tsx
@@ -1,3 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import BrandsTable from './brands-table';
+import { getBrands } from '@/lib/data/brands';
+
 export default async function BrandsPage() {
-  return <div>Brands placeholder</div>;
+  const brands = await getBrands();
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center">
+          <div>
+            <CardTitle>Edit Brands</CardTitle>
+            <CardDescription>
+              Manage the items associated with this report.
+            </CardDescription>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <BrandsTable brands={brands} />
+      </CardContent>
+      <CardFooter />
+    </Card>
+  );
 }
