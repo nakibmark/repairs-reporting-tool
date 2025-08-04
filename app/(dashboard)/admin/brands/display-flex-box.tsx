4ad3d040135b00816428ed9f5fa7d8652f62574e
Jove Luna <joveluna@gmail.com>
2025-08-04 12:07:06 -0400

    Converting item into input field on click and creating on blur function where save will be handled

--- app/(dashboard)/admin/brands/display-flex-box.tsx
+++ app/(dashboard)/admin/brands/display-flex-box.tsx
@@ -1,3 +1,8 @@
+'use client';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 const DisplayFlexBox = ({
   items,
   label,
@@ -5,18 +10,49 @@ const DisplayFlexBox = ({
   items: { id: number; name: string }[];
   label: string;
 }) => {
+  const [editingItemId, setEditingItemId] = useState<number | null>(null);
+  const [editedItemName, setEditedItemName] = useState('');
+
+  const handleItemClick = (item: { id: number; name: string }) => {
+    setEditingItemId(item.id);
+    setEditedItemName(item.name);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedItemName(e.target.value);
+  };
+
+  const handleInputBlur = (item: { id: number; name: string }) => {
+    console.log(`Updating item ${item.id} to ${editedItemName}`);
+    setEditingItemId(null);
+  };
   return (
     <div className="flex flex-row space-x-2">
       <div className="font-bold mb-2 w-48">{label}</div>
       {items.map((item) => (
-        <div
-          className="py-2 px-4 border rounded-md shadow-sm w-fit"
-          key={item.id}
-        >
-          {item.name}
+        <div key={item.id}>
+          {editingItemId === item.id ? (
+            <Input
+              type="text"
+              value={editedItemName}
+              onChange={handleInputChange}
+              onBlur={() => handleInputBlur(item)}
+              autoFocus
+              className="w-fit"
+            />
+          ) : (
+            <div
+              className="py-2 px-4 border rounded-md shadow-sm w-fit cursor-pointer"
+              onClick={() => handleItemClick(item)}
+            >
+              {item.name}
+            </div>
+          )}
         </div>
       ))}
+      <Button>Add {label}</Button>
     </div>
   );
 };
+
 export default DisplayFlexBox;
