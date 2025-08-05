a5b0c66df005c352c0a75a152fa1b0b9e2903ac2
Jove Luna <joveluna@gmail.com>
2025-08-05 11:08:41 -0400

    Implemented navigation between admin pages from home page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,3 +1,43 @@
-export default async function AdminPage() {
-  return <div>Placeholder for admin dashboard</div>;
-}
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import PageNavigation from './pageNavigation';
+
+const AdminPage = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center">
+          <div>
+            <CardTitle>Admin Home</CardTitle>
+            <CardDescription>
+              Navigate between pages used for admininstration
+            </CardDescription>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col space-y-4">
+          <PageNavigation
+            name="Partners"
+            description="Manage the information associated with partners"
+            link="/admin/partners"
+          />
+          <PageNavigation
+            name="Brands"
+            description="Manage the brands, warranty types and service types"
+            link="/admin/brands"
+          />
+        </div>
+      </CardContent>
+      <CardFooter />
+    </Card>
+  );
+};
+
+export default AdminPage;
